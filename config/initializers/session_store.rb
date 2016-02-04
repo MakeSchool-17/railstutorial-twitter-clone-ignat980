d77b9ab89f13c3d8dcf0b1b07e1f80998fdaf8f8
Ignat Remizov <ignat980@gmail.com>
2016-02-04 06:53:20 -0800

    Re-started Rails app for chapter 2

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_Hello_app_session'
+Rails.application.config.session_store :cookie_store, key: '_Toy_app_session'
