9efade5fd26479868479f85bf9e1863844a3149a
Ignat Remizov <ignat980@gmail.com>
2016-02-13 12:56:13 -0800

    Added redirect to new user page with message; Added test for successful signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -15,4 +15,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     count = 0
     assert_select 'div#error_explanation ul li', assigns(:user).errors.full_messages.count
   end
+
+  test "valid signup information" do
+    get signup_path
+    user = { name:  "Example User",
+            email: "user@example.com",
+            password:              "password",
+            password_confirmation: "password" }
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: user
+    end
+    assert_template 'users/show'
+  end
 end
